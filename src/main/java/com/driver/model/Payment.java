bfb9256ec87f8f1f5c7450b30a2c1c66bea021a1
nikhil vanamala <nickhil215@gmail.com>
2023-01-28 20:34:28 +0530

    3

--- src/main/java/com/driver/model/Payment.java
+++ src/main/java/com/driver/model/Payment.java
@@ -25,7 +25,7 @@ public class Payment {
         this.id = id;
     }
 
-    public boolean isPaymentComplated() {
+    public boolean getPaymentCompleted() {
         return paymentComplated;
     }
 
